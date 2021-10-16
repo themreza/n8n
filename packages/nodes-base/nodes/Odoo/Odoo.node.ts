0ddc38b354a3662e0d2b2d2bb5332fd2b3e31315
Mohammad Tomaraei <mohammed.tomaraei@butopea.com>
2021-10-16 16:16:02 +0200

    initiated the Odoo integration

---
+++ packages/nodes-base/nodes/Odoo/Odoo.node.ts
@@ -0,0 +1,41 @@
+import {
+	IExecuteFunctions,
+} from 'n8n-core';
+
+import {
+	IDataObject,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+} from 'n8n-workflow';
+
+import {
+	OptionsWithUri,
+} from 'request';
+
+export class Odoo implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Odoo',
+		name: 'odoo',
+		icon: 'file:odoo.svg',
+		group: ['transform'],
+		version: 1,
+		description: 'Consume Odoo API',
+		defaults: {
+			name: 'Odoo',
+			color: '#714B67',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+		],
+		properties: [
+			// Node properties which the user gets displayed and
+			// can change on the node.
+		],
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		return [[]];
+	}
+}
